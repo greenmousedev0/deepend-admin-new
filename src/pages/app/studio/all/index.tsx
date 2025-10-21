e0a8165d7c388dc68ac24052991b3160ecd713b3
destiny <desto4q@gmail.com>
2025-10-21 23:49:14 +0100

    edit studio

--- src/pages/app/studio/all/index.tsx
+++ src/pages/app/studio/all/index.tsx
@@ -1,10 +1,16 @@
 import apiClient, { type ApiResponse } from "@/api/apiClient";
 import type { Studio } from "@/api/types";
+import Modal from "@/components/dialogs-modals/SimpleModal";
 import SimpleHeader from "@/components/SimpleHeader";
+import SimpleInput from "@/components/SimpleInput";
 import SimpleLoader from "@/components/SimpleLoader";
 import SimplePaginator from "@/components/SimplePaginator";
+import { extract_message } from "@/helpers/auth";
+import { useModal } from "@/store/modals";
 import { usePagination } from "@/store/pagination";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
 
 export default function index() {
   const props = usePagination();
@@ -20,6 +26,26 @@ export default function index() {
       return resp.data;
     },
   });
+
+  const mutation = useMutation({
+    mutationFn: async (data: any) => {
+      let resp = await apiClient.post("admins/studios", data);
+      return resp.data;
+    },
+    onSuccess: () => {
+      query.refetch();
+      closeModal();
+    },
+  });
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data: any) => {
+    toast.promise(() => mutation.mutateAsync(data), {
+      loading: "Creating Studio...",
+      success: extract_message,
+      error: extract_message,
+    });
+  };
+  const { ref, showModal, closeModal } = useModal();
   if (query.isLoading) {
     return (
       <>
@@ -28,39 +54,140 @@ export default function index() {
       </>
     );
   }
+
   const items = query.data?.payload;
   return (
     <>
-      <SimpleHeader title={"All Studios"} />
+      <SimpleHeader title={"All Studios"}>
+        <button className="btn btn-primary" onClick={() => showModal()}>
+          Add Studio
+        </button>
+      </SimpleHeader>
       <div className="">
         <ul className="space-y-4">
           {items?.map((studio) => (
-            <li key={studio.id} className="card bg-base-100 shadow-xl">
-              <div className="card-body">
-                <h2 className="card-title text-2xl font-bold">{studio.name}</h2>
-                <p className="text-base-content/80">
-                  Location: {studio.location}
-                </p>
-                <p className="text-lg font-semibold">
-                  Hourly Rate: ${studio.hourlyRate}
-                </p>
-                <div className="card-actions justify-end">
-                  <div
-                    className={`badge ${
-                      studio.isAvailable ? "badge-success" : "badge-error"
-                    } text-white`}
-                  >
-                    {studio.isAvailable ? "Available" : "Not Available"}
-                  </div>
-                </div>
-              </div>
-            </li>
+            <StudioCard
+              refetch={query.refetch}
+              studio={studio}
+              key={studio.id}
+            />
           ))}
         </ul>
         <div className="mt-6">
           <SimplePaginator {...props} />
         </div>
       </div>
+      <Modal ref={ref}>
+        <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
+          <h2 className="text-xl font-bold">Add new Studio</h2>
+          <SimpleInput {...register("name")} label="Name" />
+          <SimpleInput {...register("location")} label="Location" />
+          <SimpleInput
+            {...register("hourlyRate")}
+            type="number"
+            label="Hourly Rate"
+          />
+          <button className="btn btn-primary btn-block">Submit</button>
+        </form>
+      </Modal>
     </>
   );
 }
+
+export const StudioCard = ({
+  studio,
+  refetch,
+}: {
+  studio: Studio;
+  refetch: () => void;
+}) => {
+  const delete_mutation = useMutation({
+    mutationFn: async () => {
+      let resp = await apiClient.delete(`admins/studios/${studio.id}`);
+      return resp.data;
+    },
+    onSuccess: () => {
+      refetch();
+    },
+  });
+  const { register, handleSubmit } = useForm({
+    defaultValues: {
+      name: studio.name,
+      location: studio.location,
+      hourlyRate: studio.hourlyRate,
+    },
+  });
+  const edit_mutation = useMutation({
+    mutationFn: async (data: Partial<Studio>) => {
+      let resp = await apiClient.put(`admins/studios/${studio.id}`, data);
+      return resp.data;
+    },
+    onSuccess: () => {
+      refetch();
+      closeModal();
+    },
+  });
+  const { ref, showModal, closeModal } = useModal();
+  const onSubmit = (data: Partial<Studio>) => {
+    toast.promise(() => edit_mutation.mutateAsync(data), {
+      loading: "Editing..." + studio.name,
+      success: extract_message,
+      error: extract_message,
+    });
+  };
+  return (
+    <li className="card bg-base-100 shadow-xl">
+      <div className="card-body">
+        <h2 className="card-title text-2xl font-bold">{studio.name}</h2>
+        <p className="text-base-content/80">Location: {studio.location}</p>
+        <p className="text-lg font-semibold">
+          Hourly Rate: ${studio.hourlyRate}
+        </p>
+        <div className="card-actions justify-end">
+          <button
+            className="btn btn-error"
+            onClick={() => {
+              showModal();
+            }}
+            disabled={edit_mutation.isPending}
+          >
+            Edit
+          </button>
+          <button
+            className="btn btn-error"
+            onClick={() => {
+              toast.promise(delete_mutation.mutateAsync, {
+                loading: "Deleting..." + studio.name,
+                success: extract_message,
+                error: extract_message,
+              });
+            }}
+            disabled={delete_mutation.isPending}
+          >
+            Delete Studio
+          </button>
+          {/*<div
+            className={`badge ${
+              studio.isAvailable ? "badge-success" : "badge-error"
+            } text-white`}
+          >
+            {studio.isAvailable ? "Available" : "Not Available"}
+          </div>*/}
+        </div>
+      </div>
+      <Modal ref={ref}>
+        <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
+          <h2 className="text-xl font-bold">Add new Studio</h2>
+          <SimpleInput {...register("name")} label="Name" />
+          <SimpleInput {...register("location")} label="Location" />
+          <SimpleInput
+            {...register("hourlyRate")}
+            type="number"
+            label="Hourly Rate"
+          />
+          <button className="btn btn-primary btn-block">Submit</button>
+        </form>
+      </Modal>
+    </li>
+  );
+};
