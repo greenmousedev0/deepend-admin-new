04f8d51946f6523c21d62462899e5ee48402b0be
destiny <desto4q@gmail.com>
2025-10-21 22:36:35 +0100

    addons

--- src/pages/app/food/addons/index.tsx
+++ src/pages/app/food/addons/index.tsx
@@ -2,8 +2,10 @@ import apiClient, { type ApiResponse } from "@/api/apiClient";
 import type { FoodAddon } from "@/api/types";
 import Modal from "@/components/dialogs-modals/SimpleModal";
 import SimpleHeader from "@/components/SimpleHeader";
+import SimpleInput from "@/components/SimpleInput";
 import SimpleLoader from "@/components/SimpleLoader";
 import SimplePaginator from "@/components/SimplePaginator";
+import SimpleTextArea from "@/components/SimpleTextArea";
 import { extract_message } from "@/helpers/auth";
 import { useModal } from "@/store/modals";
 import { usePagination } from "@/store/pagination";
@@ -74,12 +76,40 @@ const AddonCard = ({
       refetch();
     },
   });
+  const edit = useMutation({
+    mutationFn: async (data: any) => {
+      let resp = await apiClient.put(
+        "admins/foods/addons/categories/" + addon.id,
+        data,
+      );
+      return resp.data;
+    },
+    onSuccess: () => {
+      refetch();
+      closeModal();
+    },
+  });
+  const { ref, showModal, closeModal } = useModal();
+  const { register, handleSubmit } = useForm({
+    defaultValues: {
+      name: addon.name,
+      description: addon.description,
+    },
+  });
   const onClick = () =>
     toast.promise(mutateAsync, {
       loading: "Deleting..." + addon.name,
       success: extract_message,
       error: extract_message,
     });
+
+  const onSubmit = (data: { name: string; description: string }) => {
+    toast.promise(() => edit.mutateAsync(data), {
+      loading: "editing..." + addon.name,
+      success: extract_message,
+      error: extract_message,
+    });
+  };
   return (
     <div
       key={addon.id}
@@ -90,7 +120,14 @@ const AddonCard = ({
           {index + 1}. {addon.name}
         </h2>
         <p>{addon.description}</p>
-        <div className="ml-auto">
+        <div className="ml-auto space-x-2">
+          <button
+            className="btn btn-info"
+            onClick={showModal}
+            // disabled={isPending}
+          >
+            Edit
+          </button>
           <button
             className="btn btn-error"
             onClick={onClick}
@@ -100,6 +137,19 @@ const AddonCard = ({
           </button>
         </div>
       </div>
+      <Modal ref={ref}>
+        <form action="" className="space-y-3" onSubmit={handleSubmit(onSubmit)}>
+          <h2 className="text-xl font-bold">Edit Addon</h2>
+          <SimpleInput {...register("name")} label="Name" />
+          <SimpleTextArea {...register("description")} label="Description" />
+          <button
+            disabled={edit.isPending}
+            className="btn btn-block btn-primary"
+          >
+            Update Addon
+          </button>
+        </form>
+      </Modal>
     </div>
   );
 };
