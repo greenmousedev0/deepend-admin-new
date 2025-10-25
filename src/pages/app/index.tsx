92f291fb8cc73be8f6c150ff6dda834c34b85185
destiny <desto4q@gmail.com>
2025-10-25 23:43:08 +0100

    scrollbar

--- src/pages/app/index.tsx
+++ src/pages/app/index.tsx
@@ -3,9 +3,9 @@ import Stats from "@/components/Stats";
 
 export default function index() {
   return (
-    <div className="">
+    <>
       <Stats />
       <Charts />
-    </div>
+    </>
   );
 }
